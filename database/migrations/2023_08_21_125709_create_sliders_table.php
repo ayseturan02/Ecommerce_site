ab49cb9036ffbc2dee89dc37432318d4f0ed7ed7
ayseturan02 <ayse0025turan@gmail.com>
2023-09-04 17:47:12 +0300

    migration

---
+++ database/migrations/2023_08_21_125709_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string("image")->nullable();
+            $table->string("name");
+            $table->text("content")->nullable();
+            $table->string("link")->nullable();
+            $table->enum("status",["0","1"])->default("0");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
